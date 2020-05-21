2b870c7280d154fbcc6074a54c981f9b63ea9ed8
Humberto <humbertovieiras@hotmail.com>
2020-05-20 23:49:15 -0300

    Corrigido bug do Retrieve

--- parking/api/viewsets.py
+++ parking/api/viewsets.py
@@ -74,6 +74,13 @@ class ParkingViewSet(ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
+    def retrieve(self, request, *args, **kwargs):
+        self.permission_classes = [IsAuthenticated]
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
 
 class ParkingSpotViewSet(ModelViewSet):
 
